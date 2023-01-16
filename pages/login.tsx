1e5984afdc623f0e40592bc46b8fe067a83883a5
hyemimi <hyemi7375@gmail.com>
2023-01-16 18:00:51 +0900

    회원가입 페이지 라우팅

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,14 @@
 import LoginForm from "@/components/login/LoginForm";
 import styled from "styled-components";
-
+import Link from "next/link";
 export default function login() {
   return (
     <Wrapper>
       <LoginForm></LoginForm>
+      <h2>
+        아직 계정이 없으신가요?
+        <Link href="/signup">회원가입하기</Link>
+      </h2>
     </Wrapper>
   );
 }
