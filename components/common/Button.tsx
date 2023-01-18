25a2825a7d1a3651ca959547b97fd061b32ba99f
Ellie <naringst@gmail.com>
2023-01-18 23:47:56 +0900

    style : Add styled Button

---
+++ components/common/Button.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  /* Rectangle 39 */
+
+  width: 476px;
+  height: 60px;
+  left: 474px;
+  top: 1281px;
+
+  background: #ffffff;
+  border-radius: 10px;
+`;
