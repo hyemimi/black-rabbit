9ae0976159810d9d3c22fc5e29816a9a158d55d6
hyemimi <hyemi7375@gmail.com>
2023-01-25 19:31:34 +0900

    feat : Add filter buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,11 +17,47 @@ export default function Home() {
         <Title>검은토끼와 함께 멋진 작품을 만들어보세요</Title>
       </Banner>
       <FilterDiv>
+        <Selector name="category">
+          <option disabled selected>
+            분류
+          </option>
+          <option>전체</option>
+          <option>카메라</option>
+          <option>렌즈</option>
+          <option>캠코더</option>
+          <option>드론, 액션캠</option>
+          <option>음향 및 마이크</option>
+          <option>조명</option>
+          <option>액세서리</option>
+          <option>기타</option>
+        </Selector>
+        <Selector name="how">
+          <option disabled selected>
+            거래방법
+          </option>
+          <option>전체</option>
+          <option>픽업</option>
+          <option>배송</option>
+          <option>직거래</option>
+        </Selector>
+        <Selector name="where">
+          <option disabled selected>
+            지역
+          </option>
+          <option>전체</option>
+          <option>서울</option>
+          <option>경기</option>
+        </Selector>
+        <Input placeholder="최소" /> <h1>~</h1>
+        <Input placeholder="최대" />
+        <button>가격대 설정</button>
+      </FilterDiv>
+      <FilterDiv>
         <div>
-          <FilterButton>전체</FilterButton>
-          <FilterButton>인기</FilterButton>
-          <FilterButton>추천</FilterButton>
-          <FilterButton>최근</FilterButton>
+          <FilterButton>인기순</FilterButton>
+          <FilterButton>가격대순</FilterButton>
+          <FilterButton>리뷰순</FilterButton>
+          <FilterButton>평점순</FilterButton>
         </div>
         <div>
           <FilterButton>전체보기</FilterButton>
@@ -48,13 +84,35 @@ const Banner = styled.div`
   height: 250px;
   width: 900px;
 `;
+const Input = styled.input`
+  position: relative;
+  left: 0;
+  right: 0;
+  padding: 5px 10px;
+  padding-left: 5px;
+  font-size: 16px;
+  background-color: #c9c9c9;
+  margin: 5px;
 
+  border: none;
+`;
 const Title = styled.h1`
   padding-top: 100px;
   text-align: center;
   font-size: 30px;
 `;
 
+const Selector = styled.select`
+  width: 150px;
+  height: 35px;
+  padding: 5px 30px 5px 10px;
+  outline: 0 none;
+  color: #444;
+  background-color: #fff;
+  margin: 0;
+  border: 1px solid #aaa;
+  box-shadow: 0 1px 0 1px rgba(0, 0, 0, 0.04);
+`;
 const FilterDiv = styled.span`
   display: flex;
   justify-content: space-between;
