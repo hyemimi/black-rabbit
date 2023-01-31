4b67c159b9f9b12111d09f6e3e1631aa5fb00be8
hyemimi <hyemi7375@gmail.com>
2023-01-31 10:39:58 +0900

    feat : link target review list

--- components/detail/ReviewList.tsx
+++ components/detail/ReviewList.tsx
@@ -1,11 +1,17 @@
 import { Ireview, reviews } from "@/temp/reviews";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import Review from "./Review";
 
 export default function ReviewList() {
+  const [reviewList, setReviewList] = useState(reviews);
+  const router = useRouter();
+  const targetReviews = reviewList.filter(
+    (review) => review.Item_id === Number(router.query.id)
+  );
   return (
     <>
-      {reviews.map((review) => (
+      {targetReviews.map((review) => (
         <Review key={review.Item_id} {...review}></Review>
       ))}
     </>
