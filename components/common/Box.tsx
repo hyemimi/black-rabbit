3d5c4915dc2810470c9ffdd77353208dee1f5b04
hyemimi <hyemi7375@gmail.com>
2023-02-10 13:14:51 +0900

    style : 문의하기 페이지

--- components/common/Box.tsx
+++ components/common/Box.tsx
@@ -8,6 +8,15 @@ export const Box = styled.div<{ height?: string }>`
   border-radius: 10px;
 `;
 
+export const BoxInput = styled.input<{ height?: string; width?: string }>`
+  margin-left: 20px;
+  width: ${(props) => (props.width ? props.width : "730px")};
+  height: ${(props) => (props.height ? props.height : "300px")};
+  border: 1px solid #d9d9d9;
+  padding: 20px;
+  border-radius: 10px;
+`;
+
 export const Item = styled.div<{ width: string }>`
   margin: 10px;
   width: ${(props) => props.width};
