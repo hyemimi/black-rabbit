7d0b964b35bfcb88f6aa4651e636d92f1bc99e9f
hyemimi <hyemi7375@gmail.com>
2023-02-13 15:08:59 +0900

    style : modal style

--- components/common/modal/reviewModal.tsx
+++ components/common/modal/reviewModal.tsx
@@ -1,8 +1,22 @@
 import styled from "styled-components";
 import { ModalDiv } from "./ModalDiv";
-
+import { reviews } from "@/temp/reviews";
 export default function ReviewModal() {
-  // const { scrollY } = useScroll();
-  //console.log(scrollY.get());
-  return <ModalDiv> 모달임 </ModalDiv>;
+  const review = reviews[0];
+  return (
+    <ModalDiv>
+      <Header>
+        {" "}
+        <BigTitle>상품명</BigTitle>{" "}
+      </Header>
+    </ModalDiv>
+  );
 }
+const Header = styled.div`
+  background-color: ${(props) => props.theme.searchColor};
+`;
+const BigTitle = styled.h2`
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+`;
