2b03165c8b8104e1a9be3c5407988eab63e07dd1
hyemimi <hyemi7375@gmail.com>
2023-02-14 12:15:50 +0900

    feat : 배송지관리 페이지

---
+++ pages/addresslist.tsx
@@ -0,0 +1,63 @@
+import { Box } from "@/components/common/Box";
+import { TitleDiv } from "@/components/common/TitleDiv";
+import { Wrapper } from "@/components/common/Wrapper";
+import styled from "styled-components";
+export default function addresslist() {
+  return (
+    <Wrapper>
+      <TitleDiv>
+        <h1>배송지 관리</h1>
+      </TitleDiv>
+
+      <ItemDiv>
+        <SelectBox>
+          <input type="checkbox"></input>
+        </SelectBox>
+        <ItemBox>
+          <Item>
+            <h1>경기도 화성시</h1>
+          </Item>
+        </ItemBox>
+      </ItemDiv>
+      <ButtonDiv>
+        <Button color="delete">삭제</Button>
+        <Button>저장</Button>
+      </ButtonDiv>
+    </Wrapper>
+  );
+}
+const ItemDiv = styled.div`
+  display: flex;
+`;
+const SelectBox = styled.div`
+  width: 68px;
+  height: 118px;
+  background-color: ${(props) => props.theme.pointColor};
+  padding: 25px;
+`;
+const ItemBox = styled.div`
+  width: 800px;
+  height: 118px;
+  border: 1px solid gray;
+  display: flex;
+`;
+const Item = styled.div`
+  width: 158px;
+
+  text-align: center;
+  align-items: center;
+  padding: 20px;
+`;
+
+const ButtonDiv = styled.div`
+  display: flex;
+`;
+const Button = styled.button<{ color?: string }>`
+  background-color: ${(props) =>
+    props.color ? props.theme.searchColor : props.theme.pointColor};
+  border: none;
+  margin-left: 30px;
+  height: 50px;
+  width: 130px;
+  cursor: pointer;
+`;
