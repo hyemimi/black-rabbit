c7631f8defe3285c3fecd759392fa53b4760279c
hyemimi <hyemi7375@gmail.com>
2023-02-16 19:42:01 +0900

    style : 좋아요 페이지 정렬

--- pages/like/[id].tsx
+++ pages/like/[id].tsx
@@ -3,6 +3,8 @@ import ProductList from "@/components/home/ProductList";
 import { useEffect, useState } from "react";
 import { IItem, items } from "@/temp/items";
 import Product from "@/components/home/Product";
+import styled from "styled-components";
+
 export default function Like() {
   const [likeList, setLikeList] = useState<IItem[] | null>(null);
   useEffect(() => {
@@ -11,9 +13,17 @@ export default function Like() {
   }, []);
   return (
     <Wrapper>
-      {likeList?.map((product) => (
-        <Product key={product.Item_id} {...product} />
-      ))}
+      <BoxDiv>
+        {likeList?.map((product) => (
+          <Product key={product.Item_id} {...product} />
+        ))}
+      </BoxDiv>
     </Wrapper>
   );
 }
+
+const BoxDiv = styled.div`
+  display: flex;
+  width: 900px;
+  flex-wrap: wrap;
+`;
