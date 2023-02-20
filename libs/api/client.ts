0cb39bc993484b7fc17572f3a250ef6e77d9bf92
hyemimi <hyemi7375@gmail.com>
2023-02-20 16:34:01 +0900

    feat : react-query 설정

---
+++ libs/api/client.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BASE_API_URL = "https://api.coinpaprika.com/v1";
+
+export const instance = axios.create({
+  baseURL: BASE_API_URL,
+  withCredentials: true,
+  timeout: 15000,
+});
+
+export function get<T>(...args: Parameters<typeof instance.get>) {
+  return instance.get<T, T>(...args);
+}
+
+export function post<T>(...args: Parameters<typeof instance.post>) {
+  return instance.post<T, T>(...args);
+}
+
+export function put<T>(...args: Parameters<typeof instance.put>) {
+  return instance.put<T, T>(...args);
+}
+
+export function patch<T>(...args: Parameters<typeof instance.patch>) {
+  return instance.patch<T, T>(...args);
+}
+
+export function del<T>(...args: Parameters<typeof instance.delete>) {
+  return instance.delete<T, T>(...args);
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,19 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/theme";
 import "react-datepicker/dist/react-datepicker.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/libs/api/queryClient";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Layout />
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
