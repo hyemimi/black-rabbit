7e997bb50eeea7196aabb7f60a0a6112afd48d1f
Ellie <naringst@gmail.com>
2023-02-28 19:23:52 +0900

    feat : 로그인 성공 및 실패 시 동작 설정

--- hooks/api/auth/LoginMutation.ts
+++ hooks/api/auth/LoginMutation.ts
@@ -1,6 +1,6 @@
 import { instance } from "@/libs/api/client";
 import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
+import { useRouter } from "next/router";
 
 export interface LoginMutationRequest {
   email: string;
@@ -12,7 +12,20 @@ export interface LoginMutationResponse {
 }
 
 export default function useLoginMutation() {
-  return useMutation((data: LoginMutationRequest) =>
-    instance.post<LoginMutationResponse>("/login", data)
+  const router = useRouter();
+  return useMutation(
+    (data: LoginMutationRequest) =>
+      instance.post<LoginMutationResponse>("/login", data),
+    {
+      onSuccess: (res) => {
+        console.log(res);
+        alert("로그인 되었습니다.");
+        router.push("/");
+      },
+      onError: (error) => {
+        console.log(error);
+        alert(error);
+      },
+    }
   );
 }
