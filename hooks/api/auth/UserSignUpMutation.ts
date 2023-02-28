82a577b790aafe4d41aba650940daac2365cb5e4
Ellie <naringst@gmail.com>
2023-02-28 17:54:08 +0900

    feat : 개인 회원가입 react-query 연결 및 페이지 라우팅

--- hooks/api/auth/UserSignUpMutation.ts
+++ hooks/api/auth/UserSignUpMutation.ts
@@ -1,5 +1,6 @@
 import { instance } from "@/libs/api/client";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 
 interface UserSignupMutationRequest {
   email: string;
@@ -15,11 +16,19 @@ interface UserSignupMutationResponse {
 /*회원가입 - 개인 유저*/
 
 export default function UseUserSignupMutation() {
+  const router = useRouter();
+
   return useMutation(
     (data: UserSignupMutationRequest) => instance.post("/sign/user", data),
     {
-      onError: (error) => {
-        console.log("회원가입 실패");
+      onSuccess: (res) => {
+        console.log(res);
+        alert("회원가입 성공");
+        router.push("/signup/completed");
+      },
+      onError: (res: any) => {
+        confirm("회원가입 실패");
+        console.log(res.data.detail);
       },
     }
   );
